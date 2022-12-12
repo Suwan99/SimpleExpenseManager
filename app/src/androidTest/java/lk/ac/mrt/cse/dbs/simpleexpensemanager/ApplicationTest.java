09fd5f371f2b1ee2a486ed576302b6f26a36b799
Suwan_Sankaja <sankajahls.20@uom.lk>
2022-12-12 23:06:56 +0530

    working version

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -16,14 +16,62 @@
 
 package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistentDemoExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest{
+    private static ExpenseManager expenseManager;
+
+    @BeforeClass
+    public static void createExpenseManager(){
+        Context context = ApplicationProvider.getApplicationContext();
+        try {
+            expenseManager = new PersistentDemoExpenseManager(context);
+        } catch (ExpenseManagerException e) {
+            e.printStackTrace();
+        }
     }
+
+    @Test
+    public void addAccountTest (){
+        expenseManager.addAccount("003122","BOC","Suwan",5000.0);
+        assertTrue(expenseManager.getAccountNumbersList().contains("001T"));
+    }
+
+    @Test
+    public void addTransactionExpenseTest(){
+        int noOfTransactions = expenseManager.getTransactionLogs().size();
+        try {
+            expenseManager.updateAccountBalance("78945Z", 1, 1, 2000, ExpenseType.EXPENSE, "1000.0");
+            assertEquals(noOfTransactions +1,expenseManager.getTransactionLogs().size());
+        } catch (InvalidAccountException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void addTransactionIncomeTest(){
+        int noOfTransactions = expenseManager.getTransactionLogs().size();
+        try {
+            expenseManager.updateAccountBalance("78945Z", 1, 1, 2000, ExpenseType.INCOME, "1000.0");
+            assertEquals(noOfTransactions +1,expenseManager.getTransactionLogs().size());
+        } catch (InvalidAccountException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
